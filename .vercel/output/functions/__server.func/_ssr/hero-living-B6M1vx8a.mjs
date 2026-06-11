bb63026f5d12c145bac7651d284eb7f212b61e30
olovi-rg <olovirg@example.com>
2026-06-12 00:46:36 +0530

    switch to npm install

---
+++ .vercel/output/functions/__server.func/_ssr/hero-living-B6M1vx8a.mjs
@@ -0,0 +1,4 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/hero-living-B6M1vx8a.js
+var hero_living_default = "/assets/hero-living-BurGap2I.jpg";
+//#endregion
+export { hero_living_default as t };
